b421b1526cbfcafe3d4ab04ead8c66424a05f5e4
sungwookang <sungwookang11@gmail.com>
2022-08-16 10:26:21 +0900

    0000 완성

---
+++ src/0000.js
@@ -0,0 +1,19 @@
+import './style.css';
+import bigLogo from './image source/bigLogo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="background00">
+        <div className="imgBox00">
+          <img src={bigLogo}></img>
+        </div>
+        <button className="startBtn00">
+          <span className="startSpan00">시작하기</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './0000';
 
 test('renders learn react link', () => {
   render(<App />);
