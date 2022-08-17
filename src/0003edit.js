23fae525a34dd760e44befc5d8a47fbaf1be486b
sungwookang <sungwookang11@gmail.com>
2022-08-17 10:27:33 +0900

    0003edit 완성

--- src/0003edit.js
+++ src/0003edit.js
@@ -12,9 +12,32 @@ function App() {
           </div>
           <div className="dayPart03e">
             <span className="birthchk03e">OO님의 생일은 언제인가요?</span>
-            <input className="month03e" type=""></input>
+            <div className='selectBox03e'>
+              <div className='monthSide03e'>
+                <select className='month03e' name='month'>
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                  <option value={3}>3</option>
+                  <option value={4}>4</option>
+                  <option value={5}>5</option>
+                  <option value={6}>6</option>
+                  <option value={7}>7</option>
+                  <option value={8}>8</option>
+                  <option value={9}>9</option>
+                  <option value={10}>10</option>
+                  <option value={11}>11</option>
+                  <option value={12}>12</option>
+                </select>
+                <span className='월'>월</span>
+              </div>
+              <div className='daySide03e'>
+                <input type='number' className='day03e' min='1' max='31'></input>
+                <span className='일'>일</span>
+              </div>
+              
+            </div>
           </div>
-          
+          <button className='saveBtn03e'>저장</button>
         </div>
       </div>
     </div>
