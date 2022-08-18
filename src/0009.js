0b57753c8e9a95d87c5521664ad09a2003c34ce3
sungwookang <sungwookang11@gmail.com>
2022-08-18 12:46:28 +0900

    0009 편지 슬라이드 빼고 완성

--- src/0009.js
+++ src/0009.js
@@ -1,19 +1,26 @@
 import './0009.css';
 import backarrow from './image source/backarrow.png';
+import leftarrow from './image source/left-arrow(9).png';
+import rightarrow from './image source/right-arrow(9).png';
+
 
 function App() {
   return (
     <div className="App">
       <div className="background09">
         <div className='backPrat09'>
-            <img className='backBtn09' src={backarrow}></img>
+          <img className='backBtn09' src={backarrow}></img>
         </div>
-        <div className='letterBox09'>
-            <textarea className='letter09'>
-                
-            </textarea>
+        <div className="container09">
+          <div className='letterBox09'>
+            <textarea className='letter09'></textarea>
+          </div>
+          <div className="counting09">
+            <img src={leftarrow}></img>
+            <div className="pageNum09">1 / 23</div>
+            <img src={rightarrow}></img>
+          </div>
         </div>
-
       </div>
     </div>
   );
