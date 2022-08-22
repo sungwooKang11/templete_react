a04f6687aaa54a8f56c06cb84d522a9bcdb291e9
sungwookang <sungwookang11@gmail.com>
2022-08-22 12:30:39 +0900

    ..

--- src/0001.js
+++ src/0001.js
@@ -1,13 +1,10 @@
 import './0001.css';
-import backarrow from './image source/backarrow.png';
-import leftarrow from './image source/left-arrow(9).png';
-import rightarrow from './image source/right-arrow(9).png';
 
 
 function App() {
   return (
     <div className="App">
-      <div className="background09">
+      <div className="background01">
         <div className="login01">
             <span className='canMake01'>롤링페이퍼를 만들 수 있어요!</span>
             <span className='loginSpan01'>로그인하고 <br></br> 시작하기</span>
