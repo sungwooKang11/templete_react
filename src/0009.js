e01f82a5347dc89a3f39edae3f3a991f05c4d512
sungwookang <sungwookang11@gmail.com>
2022-08-25 23:19:54 +0900

    border= none

--- src/0009.js
+++ src/0009.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import './0009.css';
 import backarrow from './image source/backarrow.png';
 import leftarrow from './image source/left-arrow(9).png';
@@ -5,6 +7,7 @@ import rightarrow from './image source/right-arrow(9).png';
 
 
 function App() {
+
   return (
     <div className="App">
       <div className="background09">
@@ -12,10 +15,12 @@ function App() {
           <img className='backBtn09' src={backarrow}></img>
         </div>
         <div className="container09">
-          <div className='letterBox09'>
-            <textarea className='letter09'></textarea>
-            <textarea className='letter09'></textarea>
-            <textarea className='letter09'></textarea>
+          <div className="letterCon09">
+            <div className='letterBox09'>
+              <textarea className='letter09'></textarea>
+              <textarea className='letter09'></textarea>
+              <textarea className='letter09'></textarea>
+            </div>
           </div>
           <div className="counting09">
             <img className="leftBtn09" src={leftarrow}></img>
