5cd9eecf6e5ab04470d620fb08802fe569b00616
sungwookang <sungwookang11@gmail.com>
2022-08-26 11:44:48 +0900

    0006delete 완성

---
+++ src/0006delete.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import './0006delete.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="background09">
+        {/* <span className='span06out'>opacity 확인용 지워도 됨</span> */}
+        <div className="logOutModal06">
+            <span className="logoutQ06">꽃병을 삭제하시겠어요?</span>
+            <div className="buttonCon06">
+                <button className="O06">
+                    <div className="circle06"></div>
+                </button>
+                <button className="X06"></button>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './0006out';
+import App from './0006delete';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
