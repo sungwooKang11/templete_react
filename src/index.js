4e15e710332416fde84c91f945fa149e0215dada
sungwookang <sungwookang11@gmail.com>
2022-08-26 11:41:44 +0900

    0006out 완성

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './0009';
+import App from './0006out';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
